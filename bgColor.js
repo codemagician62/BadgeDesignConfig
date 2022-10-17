336d7554c52e779ead8bfd385af5ae9fb32a44d8
jbush <jadariantech@gmail.com>
2022-10-17 12:55:31 -0500

    first commit

--- bgColor.js
+++ bgColor.js
@@ -1,55 +1,51 @@
-function startConfetti() {
-  unlayer.registerTool({
-    name: "my_tool",
-    label: "Background Color",
-    icon: "fa-palette",
-    supportedDisplayModes: ["web", "email"],
-    options: {
-      colors: {
-        // Property Group
-        title: "Colors", // Title for Property Group
-        position: 1, // Position of Property Group
-        options: {
-          backgroundColor: {
-            // Property: backgroundColor
-            label: "Background Color", // Label for Property
-            defaultValue: "#FF0000",
-            widget: "color_picker", // Property Editor Widget: color_picker
-          },
+unlayer.registerTool({
+  name: "my_tool",
+  label: "Background Color",
+  icon: "fa-palette",
+  supportedDisplayModes: ["web", "email"],
+  options: {
+    colors: {
+      // Property Group
+      title: "Colors", // Title for Property Group
+      position: 1, // Position of Property Group
+      options: {
+        backgroundColor: {
+          // Property: backgroundColor
+          label: "Background Color", // Label for Property
+          defaultValue: "#FF0000",
+          widget: "color_picker", // Property Editor Widget: color_picker
         },
       },
     },
-    values: {},
-    renderer: {
-      Viewer: unlayer.createViewer({
-        render(values) {
-          return "";
-        },
-      }),
-      exporters: {
-        web: function (values) {
-          return "";
-        },
-        email: function (values) {
-          return "";
-        },
+  },
+  values: {},
+  renderer: {
+    Viewer: unlayer.createViewer({
+      render(values) {
+        return "";
       },
-      head: {
-        css: function (values) {
-          // console.log("values => ", values)
-          // console.log("values._meta.htmlID => ", values._meta.htmlID)
-          // return `#${values._meta.htmlID} { background-color: ${values.backgroundColor}; color: ${values.textColor}; }`
-          return `#u_body { background-color: ${values.backgroundColor} !important; }`;
-        },
-        js: function (values) {
-          return 'console.log("Tool JavaScript");';
-        },
+    }),
+    exporters: {
+      web: function (values) {
+        return "";
+      },
+      email: function (values) {
+        return "";
       },
     },
-    validator(data) {
-      return [];
+    head: {
+      css: function (values) {
+        // console.log("values => ", values)
+        // console.log("values._meta.htmlID => ", values._meta.htmlID)
+        // return `#${values._meta.htmlID} { background-color: ${values.backgroundColor}; color: ${values.textColor}; }`
+        return `#u_body { background-color: ${values.backgroundColor} !important; }`;
+      },
+      js: function (values) {
+        return 'console.log("Tool JavaScript");';
+      },
     },
-  });
-}
-startConfetti();
-
+  },
+  validator(data) {
+    return [];
+  },
+});
